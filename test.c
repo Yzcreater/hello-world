f38e19855e2f95c85b5044ff4bdf5c5cb3fd9db5
Yzcreater <119487610+Yzcreater@users.noreply.github.com>
2022-12-14 22:43:38 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,58 +1,173 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-#include <string.h>
-//n的阶乘
-//int Fac2(int n) {
-//	if (n < 1) {
-//		return 1;
-//	}
-//	else {
-//		return n * Fac2(n-1);
-//		
-//	}
-//
-//
-//
-//}
+#include<stdio.h>
+#include<string.h>
 //int main() {
-//	int n = 0;
-//	int ret = 0;
-//	scanf("%d",&n);
-//	ret = Fac2(n);
-//	printf("%d\n",ret);
+//	//int arr[10] = {1,2,3};//不完全初始化，后面默认的是0；局部变量
+//	//char arr2[5] = { 'a','b'};
+//	//char arr3[5] = "ab";
+//	char arr4[] = "abcdef";
+//	printf("%d\n",sizeof(arr4));//7
+//	//sizeof 计算arr4所占空间的大小
+//	//strlen 求字符串长度
+//	printf("%d\n",strlen(arr4));//6
+//
+//
 //	return 0;
 //}
-//
-//斐波那契数列
-//int Fib(int n) {
-//
-//	if (n <= 2)
-//		return 1;
-//	else
-//		return Fib(n - 1) + Fib(n - 2);
-//	
-//
+//int main() {
+//	char arr[] = "abc";
+//	char arr1[3] = {'a','b','c'};
+//	printf("%d\n",sizeof(arr));
+//	printf("%d\n",sizeof(arr1));
+//	printf("%d\n",strlen(arr));
+//	printf("%d\n",strlen(arr1));
+//	return 0;
 //}
-int Fib(int n) {
-	int a = 1;
-	int b = 1;
-	int c = 0;
-	while (n>2)
-	{
-		c = a + b;
-		a = b;
-		b = c;
-		n--;
-	}
-	return c;
-
-
-}
-int main() {
-	int n = 0;
-	int ret = 0;
-	scanf("%d",&n);
-	ret = Fib(n);
-	printf("%d\n",ret);
-	return 0;
-}
+//int main() {
+//	//char arr[] = "abcdef";
+//	////printf("%c\n",arr[3]);
+//	//int i = 0;
+//	//for ( i = 0; i < 6; i++)
+//	//{
+//	//	printf("%c ",arr[i]);
+//	//}
+//	int arr[] = {1,2,3,4,5,6,7,8,9,0};
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for ( i = 0; i < sz; i++)
+//	{
+//		printf("%d ",arr[i]);
+//	}
+//	return 0;
+//}
+//int main() {
+//	int arr[] = {1,2,3,4,5,6,7,8,9,10};
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for ( i = 0; i < sz; i++)
+//	{
+//		printf("&arr=[%d] = %p\n",i,&arr[i]);
+//	}
+//
+//	return 0;
+//}
+//int main() {
+//	//int arr[3][4] = {1,2,3,4,5};
+//
+//	int arr1[3][4] = {{1,2,3},{4,5}};
+//	int i = 0;
+//	for ( i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for ( j = 0; j < 4; j++)
+//		{
+//			printf("%d ",arr1[i][j]);
+//		}
+//		printf("\n");
+//	}
+//
+//	//int arr2[][4] = {{1,2,3},{4,5}};//行可以省略，列不行
+//
+//
+//	//char ch[5][6];
+//
+//	return 0;
+//}
+//int main() {
+//	//int arr[3][4] = {1,2,3,4,5};
+//
+//	int arr1[3][4] = { {1,2,3},{4,5} };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("&arr1[%d][%d]=%p\n ", i, j, &arr1[i][j]);
+//		}
+//
+//
+//		//int arr2[][4] = {{1,2,3},{4,5}};//行可以省略，列不行
+//
+//	}
+//	//char ch[5][6];
+//
+//	return 0;
+//}
+//void bubble_sort(int arr[],int sz) {
+//	//确定排序的趟数
+//	int i = 0;
+//	//int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	for ( i = 0; i < sz-1; i++)
+//	{	
+//		int flag = 1;//假设这次的数据已经有序
+//		//每一趟冒泡排序的内容
+//		int j = 0;
+//		for ( j = 0; j < sz-1-i; j++)
+//		{
+//			if (arr[j] > arr[j + 1]) {
+//				int tem = 0;
+//				tem = arr[j];
+//				arr[j] = arr[j+1];
+//				arr[j+1] = tem; 
+//				flag = 0;//本趟排序的数字其实不完全有序
+//			}
+//		}
+//		if (flag == 1) {
+//			break;
+//		}
+//	}
+//}
+//int main() {
+//	int arr[] = {9,8,7,6,5,4,3,2,1,0};
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//对arr进行排序
+//	bubble_sort(arr,sz);//冒泡排序函数
+//	for ( i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//
+//
+//
+//	return 0;
+//}
+//int main() {
+//	int arr[] = { 1,2,3,4,5,6,7 };
+//	printf("%p\n",arr);
+//	printf("%p\n",&arr[0]);
+//	printf("%d\n",*arr);
+//
+//
+//
+//	return 0;
+//}
+//void bobble_sort(int arr[],int sz) {
+//	int i = 0;
+//	for ( i = 0; i < sz-1; i++)
+//	{
+//		int j = 0;
+//		for ( j = 0; j < sz-1-i; j++)
+//		{
+//			if (arr[j] > arr[j + 1]) {
+//				int tem = arr[j + 1];
+//				arr[j + 1] = arr[j];
+//				arr[j] = tem;
+//			
+//			}
+//		}
+//	}
+//}
+//int main() {
+//	int arr[] = { 9,8,7,6,5,4,3,2,1,0,};
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bobble_sort(arr,sz);
+//	for ( i = 0; i < sz; i++)
+//	{
+//		printf("%d ",arr[i]);
+//	}
+//	return 0;
+//}
---
+++ test2.c
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//测试三子棋
+#include <stdio.h>
+#include <string.h>
+#include "game.h"
+void menu() {
+
+	printf("*************************************\n");
+	printf("********1.play  0.exit***************\n");
+	printf("*************************************\n");
+
+}
+void game() {
+	char ret = 0;
+	//数组存放走出的棋盘信息
+	char board[ROW][COL] = {0};//全部空格
+	//初始化棋盘
+	InitBoard(board,ROW,COL);
+	//打印棋盘
+	DisplayBoard(board, ROW, COL);
+	//下棋
+	while (1)
+	{
+		//玩家下
+		PlayerMove(board,ROW,COL);
+		DisplayBoard(board, ROW, COL);
+		//判断玩家是否赢
+		ret = Iswin(board, ROW, COL);
+		if (ret != 'C') {
+			break;
+		}
+		//电脑下
+		ComputerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		//判断电脑是否赢
+		ret = Iswin(board, ROW, COL);
+		if (ret != 'C') {
+			break;
+		}
+
+	}
+
+	if (ret == '*') {
+		printf("玩家赢了\n");
+	}
+	else if (ret == '#') {
+	
+		printf("电脑赢了\n");
+	}
+	else {
+		printf("平局\n");
+	}
+	
+
+
+}
+void test() {
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择：>");
+		scanf("%d",&input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，重新输入\n");
+			break;
+		}
+	} while (input);
+	
+
+}
+
+int main() {
+
+	test();
+	return 0;
+}
